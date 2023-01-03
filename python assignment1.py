4405f654b80f1b5cdc04d7a693db3340aa763ed4
Sanunandedkar <121793881+Sanunandedkar@users.noreply.github.com>
2023-01-03 15:23:03 +0530

    Add files via upload

---
+++ python assignment1.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Assignment by Sanika Nandedkar
+
+# # Q1. Write a python programe to calculate the length of tha string.
+
+# In[1]:
+
+
+s=input("Enter the string here")
+
+
+# In[2]:
+
+
+print(s)
+
+
+# # length of the string
+
+# In[3]:
+
+
+l1=len(s)
+
+
+# In[4]:
+
+
+print("length of the string :",l1)
+
+
+# # Q2. Python programe to calculate the square root.
+
+# In[5]:
+
+
+import math 
+
+
+# In[6]:
+
+
+print("the square root of 4 is ",math.sqrt(4))#pass the decimal number
+
+
+# # Q3. Write a python programe to convert the temperature in celcius to temperature in fahrenheit .
+
+# In[7]:
+
+
+celcius=45
+
+
+# In[8]:
+
+
+fahrenheit=celcius*9/5+32
+
+
+# In[9]:
+
+
+print(fahrenheit)
+
+
+# # Q4. Write a data type needed for given data (10,4.5,2+6j).
+
+# In[10]:
+
+
+num=10
+
+
+# In[11]:
+
+
+type(num)
+
+
+# In[12]:
+
+
+num=4.5
+
+
+# In[13]:
+
+
+type(num)
+
+
+# In[14]:
+
+
+num=2+6j
+
+
+# In[15]:
+
+
+type(num)
+
+
+# In[ ]:
+
+
+
+
